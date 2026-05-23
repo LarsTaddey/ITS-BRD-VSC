9b9a8cdd9af92ff489f0b2075ef2c99a839058c5
LarsTaddey <wlp676@haw-hamburg.de>
2026-05-23 19:00:43 +0200

    Aufgabe 5: Teilfunktion Sieb in main.s implementiert und Datenstruktur in eine Excel-Tabelle geschrieben

--- Programs/PrimzahlSiebASM/Src/main.s
+++ Programs/PrimzahlSiebASM/Src/main.s
@@ -1,100 +1,117 @@
-;******************** (C) COPYRIGHT HAW-Hamburg ********************************
-;* File Name          : main.s
-;* Author             : Silke Behn	
-;* Version            : V1.0
-;* Date               : 01.06.2021
-;* Description        : This is a simple main.
-;					  :
-;					  : Replace this main with yours.
-;
-;*******************************************************************************
-    EXTERN initITSboard
-    EXTERN lcdPrintS            ;Display ausgabe
-    EXTERN GUI_init
-;	EXTERN TP_Init
+;************************************************
+;* Beginn der globalen Daten *
+;************************************************
+                   AREA MyData, DATA, align = 2
 
-;********************************************
-; Data section, aligned on 4-byte boundery
-;********************************************
-	
-	AREA MyData, DATA, align = 2
-	
-	    GLOBAL text
+LIMIT 			EQU		31									; Die gesetzte Obergrenze des Bereichs (hier 1000)
+PrimZahlSieb 	FILL 	LIMIT + 1							; Der Speicherbereich, der das Sieb repräsentiert 0 = false, 1 = true
+Primzahlen		FILL 	(LIMIT / 2) * 4						; Der Speicherbereich, der die gefundenen Primzahlen enthält. 
+															; Länge wird approximiert, um etwas Speicher zu sparen
 
-DEFAULT_BRIGHTNESS DCW  800
+;***********************************************
+;* Beginn des Programms *
+;************************************************
+    AREA |.text|, CODE, READONLY, ALIGN = 3
+; ----- S t a r t des Hauptprogramms -----
+        		EXPORT main
+        		EXTERN initITSboard
+main    		PROC
 
-text	DCB	"Hallo liebes TI-Labor (asm-project)",0
+;------------------------------------------------
+; Teilfunktion Sieb
+;------------------------------------------------
+Sieb		
+				ldr		R0, =PrimZahlSieb					; Startadresse des Siebs wird in R0 geladen
+				ldr 	R1, =LIMIT							; Der Wert von Limit wird in R1 geladen
+
+;------------------------------------------------			
+; for-Schleife: Initialisieren
+;------------------------------------------------	
+for_init
+				mov 	R5, #2								; Setze den Zähler R5 auf 2
+				mov 	R6, #1								; Setze R6, zum setzen der Bytes im Sieb, auf 1
+until_init
+				cmp 	R5, R1								; Schleifenabbruch, wenn R5 größer als R1 (LIMIT) ist
+				bgt 	enddo_init							; Sonst springe in die Schleife
+do_init
+				strb	R6, [R0,R5]							; Setze das Byte an der Stelle ab R0 + R5, im Speicher, auf 1
+step_init
+				add 	R5, #1								; Erhöhe den Zähler um 1
+				b 		until_init							; Springe zum Schleifenanfang
+enddo_init
+
+;------------------------------------------------			
+; Äußere for-Schleife: Finde potentielle Primzahlen 
+;------------------------------------------------	
+for_sieben
+				mov 	R5, #2								; Setze den Zähler wieder auf 2
+				mov 	R6, #0								; Setze R6 auf 0 zum markieren
+until_sieben
+				mul 	R3, R5, R5							; Berechne das Quadrat aus der momentanen Stelle (Zähler) und schreibe in R3
+				cmp		R3, R1								; Vergleiche, ob R3 größer als R1
+				bgt 	enddo_sieben						; Wenn R3 größer R1, ist man fertig. Sonst gehe in die Schleife
+do_sieben
+				ldrb 	R2, [R0, R5]						; Lade den Wert aus dem Byte von R0 + R5
+				b 		if_sieben							; Springe in den If-Case
+
+step_sieben							
+				b 		NaechstePrimzahl					; Springe zu NaechstePrimzahl
+enddo_sieben
+				b 		Abspeichern							; Sieb ist fertig, springe zu Abspeichern
+
+; Wenn gefundener Wert gleich 1 ist, dann markiere Vielfache, 
+; sonst finde das nächste nichtgestrichene Byte
+;------------------------------------------------	
+
+if_sieben
+				cmp 	R2, #1								; Vergleiche, ob R2 gleich 1 ist, also R2 eine gültige Primzahl
+				beq 	then_sieben							; Wenn R2 == 1, dann fang mit dem markieren der Vielfachen an,
+				b 		else_sieben							; sonst finde die nächste potentielle Primzahl
+then_sieben	
+				b		for_markieren						; Springe zu for_markieren
+
+else_sieben		b 		endif_sieben						; Springe zu endif_sieben
+
+endif_sieben	b 		NaechstePrimzahl					; Springe zu NaechstePrimzahl
+
+;------------------------------------------------			
+; Innere for-Schleife: Markiere Vielfache
+;------------------------------------------------	
+for_markieren
+
+until_markieren
+				cmp 	R3, R1								; Vergleiche, ob R3 größer R1 ist
+				bgt 	enddo_markieren						; Wenn R3 > R1, dann ist das Markieren dieser Vielfache abgeschlossen,
+															; sonst gehe in die Schleife
+do_markieren
+				strb	R6, [R0, R3]						; Speicher den Wert 0 im Byte an der Stelle R0 + R3. Also markiere Vielfache (setze auf 0)
+step_markieren
+				add 	R3, R5								; Addiere zu dem Vielfachen die eigentliche Zahl
+				b 		until_markieren						; Springe zum Schleifenkopf
+enddo_markieren
+				b 		NaechstePrimzahl					; Wenn R3 > R1, dann finde die nächste potentielle Primzahl
+
+;------------------------------------------------			
+; Unterfunktion NaechstePrimzahl
+; Erhöht den Zähler um 1
+;------------------------------------------------	
+NaechstePrimzahl
+				add 	R5, #1								; Erhöhe den Zähler um 1, damit die nächste potentielle Primzahl gefunden werden kann
+				b 		until_sieben						; Springe zum Schleifenkopf von sieben
 
 
-;********************************************
-; Code section, aligned on 8-byte boundery
-;********************************************
 
-	AREA |.text|, CODE, READONLY, ALIGN = 3				
 
-;--------------------------------------------
-; main subroutine
-;--------------------------------------------
-	EXPORT main [CODE]
-	
-main	PROC				
-        BL initITSboard
-		ldr r1, =DEFAULT_BRIGHTNESS
-		ldrh r0, [r1]
-		bl GUI_init
-		mov r0, #0x00
-;		bl TP_Init
-		
-		LDR	r0,=text
-        BL  lcdPrintS
+;---Ignorieren ist nicht Teil der Aufgabe 5---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-;forever	b	forever		; nowhere to retun if main ends		
-		ENDP
-	
-		ALIGN
-       
-		END
+;------------------------------------------------			
+; Teilfunktion Abspeichern
+;------------------------------------------------
+Abspeichern
 
-; Labels benötigter Speicherbereiche setzen.
-; Benötigte Labels: 	
-	; PrimZahlSieb: Speicherbereich für die Werte, ob eine Zahl eine Primzahl ist. Repräsentiert durch die Adresse relativ zu PrimZahlSieb (Zahl 2 = PrimZahlSieb + 2)
-	; Primzahlen: Speicherbereich für die gefundenen Primzahlen als tatsächliche Zahlen (=PrimZahlSieb + Limit + 1)
-	; Sieb: Zeigt auf den Beginn der Funktion, um Primzahlen mit dem Verfahren nach Erathosthenes zu finden
-	; Abspeichern: Schreibt die gefunden Primzahlen in den Speicher ab Abspeichern
-
-		; Für den Speicherbereich mit dem Label PrimZahlSieb müssen so viele Bytes gesetzt werden, wie das Limit (1000 Bytes)
-		; Für den Speicherbereich mit dem Label Abspeichern müssen so viele Bytes, mit Primzahlen gefüllt werden, wie Primzahlen gefunden worden
-
-		; Sieb: 
-			; Soll erst alle benötigten Bytes ab PrimZahlSieb auf 1 s setzen. 1 repräsentiert ein true und 0 ein false. Benötigte Bytes = Anzahl gefundener Primzahlen
-			; Wenn das Quadrat der gefundenen Primzahl größer als das Limit ist, ist das Sieben fertig und es kann zu Abspeichern gesprungen werden
-			; Die kleinste gefundene Zahl soll direkt als Primzahl gespeichert werden
-			; Danach wird das Quadrat der momentanen Zahl (mit 1) auf 0 gesetzt
-			; Dann werden alle weiteren vielfachen der Zahl auf 0 gesetzt (Adresse von PrimZahlSieb + Offset soll 0 sein)
-			; Es wird die nächste potentielle Primzahl gesucht (das nächste Byte das 1 enthält)
-			; Dabei werden bereits auf 0 gesetzte Stellen nicht mehr betrachtet
-			; Dann wird zu Sieb gesprungen bis die größte Primzahl erreicht ist und alle Vielfachen gestrichen wurden
-		
-		; Für Sieb benötigt man einige Register.
-			; r0 enthält Basisadresse unseres Siebs
-			; r1 enthält aktuelles Limit
-			; r2 enthält aktuelle Primzahl
-			; r3 enthält das quadrat einer Primzahl und das vielfachen
-			; r4 zur berechnung temporärer Adressen oder Primzahlen
-			; r5 als Zählregister
-			; r6 zur markierung von vielfachen und zur Initialisierung des Siebs
-
-		; Abspeichern:
-				
-			; Soll den gesiebten Speicherbereich analysieren und gefundene Primzahlen in eigenem Speicherbereich schreiben
-			; Dabei ist der Zähler der tatsächliche Wert der gesuchten Primzahl, da an der Stelle die 1 im Byte gefunden wurde
-			; Das passiert solange, bis der Zähler einmal durch das ganze Sieb gegangen ist
-			; In den neuen Speicherbereich werden dann die gefundenen Primzahlen in den Speicher hineingeschrieben
-			; Das Ergebnis ist ein Speicherbereich mit allen gefunden Primzahlen aus dem Bereich 2 bis n (hier n = 1000)
-			; Wenn das Sieb fertig durchlaufen ist, dann ist das Programm fertig
-
-		; Abspeichern braucht weniger Register
-			; r0 enthält Basisadresse des Siebs
-			; r1 enthält aktuelles Limit
-			; r4 enthält Adresse des neuen Speicherbereichs
-			; r5 als Zählregister
+forever         
+				; Existiert nur für den Breakpoint zum debuggen
+				mov 	R0, #0        						
+				b forever             
+                ENDP
+                END
